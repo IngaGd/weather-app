5487e69bdb1170ae1ef3cfa73f872662a305522a
Inga <inga.gudaite@gmail.com>
2023-08-02 11:52:15 +0300

    Main structure of react and sass components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from './pages/Home';
+import './style/main.scss';
+
+function App() {
+    return (
+        <div>
+            <Home />
+        </div>
+    );
+}
+
+export default App;
